856948721de5c7e7918b3dae0f655b7c006fc090
nomayen <nomayen.ohin@gmail.com>
2022-08-02 14:08:40 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,38 @@
+// while loop
+
+// var roastGiven =0;
+// while (roastGiven < 7) {
+//     console.log('roast den please');
+//     roastGiven++;
+// }
+
+// for loop
+
+// for (roastGiven =0; roastGiven < 7; roastGiven++) {
+//     console.log('roast den please '); 
+// }
+
+
+
+// simple version for loop
+
+/* console.log('simple for loop');
+for (var i =0; i < 7; i++) {
+     console.log(i);
+} */
+
+// 30 pojonto count using for loop
+/* for (var i =0; i <= 30; i++) {
+    console.log(i);
+} */
+
+// jot shonkha
+/* for (var i =0; i <= 10; i +=2) {
+    console.log(i);
+} */ 
+
+// bijot shonklha
+
+for (var i =1; i <= 30; i +=2) {
+    console.log(i);
+} 
