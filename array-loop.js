b7e0313a82f4d590ab3a461e545893a284e9029d
nomayen <nomayen.ohin@gmail.com>
2022-08-02 14:48:13 +0600

    arrayt-loop

---
+++ array-loop.js
@@ -0,0 +1,22 @@
+/* for (var i =0; i < 10; i++) {
+    console.log(i);   
+} */
+
+// target display every element og an array
+
+/* var numbers = [45, 87, 89, 56, 32, 54, 25, 35, 65, 85, ];
+
+for ( var i = 0; i < numbers.length ; i++) {
+    var number = numbers[i] ;
+   console.log(number);
+    
+}
+ */
+
+
+ var numbers = [45, 87, 89, 56, 22, 33, 44, 35, 65, 85, ];
+ for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number);
+    
+ }
