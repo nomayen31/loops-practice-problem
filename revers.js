8fd08f5bc4ecd43597845256779b6c47f2bb9860
nomayen <nomayen.ohin@gmail.com>
2022-08-02 16:30:37 +0600

    revers way

---
+++ revers.js
@@ -0,0 +1,30 @@
+/* var num = 1;
+while (num <= 10) {
+    console.log(num);
+    num++;
+} */
+
+
+// revers way 
+
+/* var num = 10;
+while (num >= 1) {
+     console.log(num);
+    num--
+ }
+*/
+
+
+// normally for loop 1 to 10
+
+/* for (var i  = 0; i <= 0 ; i++ ){
+   console.log(i);
+}
+ */
+
+
+//  revers for loop 10 to 1 revers way  
+
+for (var i  = 10; i >= 0 ; i--){ 
+   console.log(i);
+}
